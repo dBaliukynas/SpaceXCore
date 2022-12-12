9a3da7de844e65e199b07adb0f1593d08f9bf98e
dBaliukynas <domas.baliukynas@gmail.com>
2022-12-12 16:46:41 +0200

    Add initialized values in filtering

--- SpaceXCore/SpaceXCore/wwwroot/js/FilteringRockets.js
+++ SpaceXCore/SpaceXCore/wwwroot/js/FilteringRockets.js
@@ -8,17 +8,18 @@ function rocketFilters() {
         height: null,
         costPerLaunch: null,
         fetchData() {
+            console.log(this.name);
          
         },
         search() {
             const searchParams = new URLSearchParams();
             const url = "/rockets";
 
-            this.reusableFS && searchParams.set("reusable-fs", this.reusableFS);
-            this.notReusableFS && searchParams.set("not-reusable-fs", this.notReusableFS);
             this.name && searchParams.set("name", this.name);
             this.height && searchParams.set("height", this.height);
             this.costPerLaunch && searchParams.set("cost-per-launch", this.costPerLaunch);
+            this.reusableFS && searchParams.set("reusable-fs", this.reusableFS);
+            this.notReusableFS && searchParams.set("not-reusable-fs", this.notReusableFS);
 
             const queryString = searchParams.toString();
             const fullUrl = `${url}?${queryString}`;
